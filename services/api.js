27519949f95d3677d7c9243e5378ad57fe4b4e46
Sanjana B <sanjana.b0831@gmail.com>
2026-03-19 15:16:20 +0530

    feat: add request interceptor to include authorization token for authenticated users

--- services/api.js
+++ services/api.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { auth } from '../src/firebase/config';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const user = auth.currentUser;
+
+    if (user) {
+      const token = await user.getIdToken();
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
